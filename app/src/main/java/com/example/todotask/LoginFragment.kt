b2ea17bc69e9ecababa43fd9b29597293fbbe584
Softzino <almuheetu@gmail.com>
2024-09-13 10:50:40 +0600

    number & password validation implement

--- app/src/main/java/com/example/todotask/LoginFragment.kt
+++ app/src/main/java/com/example/todotask/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.todotask.databinding.FragmentLoginBinding
 
 
@@ -27,10 +28,51 @@ class LoginFragment : Fragment() {
     }
 
 
+    private fun showPhoneNumberError(number: String) {
+        binding.numberErrorTv.text = number
+        binding.numberErrorTv.visibility = View.VISIBLE
+    }
+
+    private fun showPasswordError(password: String) {
+        binding.passErrorTv.text = password
+        binding.passErrorTv.visibility = View.VISIBLE
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbarInclude.toolbarTitle.text = "Login"
         binding.toolbarInclude.backButton.visibility = View.GONE
+        binding.loginBtn.setOnClickListener {
+            val phoneNumber = binding.phoneNumberEt.text.toString()
+            val password = binding.editTextPassword.text.toString()
+            if (isValidPhoneNumber(phoneNumber)) {
+                if (phoneNumber == "01789803342") {
+                    if (isValidPassword(password)) {
+                        if (password == "123456") {
+                            val action =
+                                LoginFragmentDirections.actionLoginFragmentToTodoTaskFragmentNavId()
+                            findNavController().navigate(action)
+                        } else {
+                            showPasswordError("Invalid Password")
+                        }
+                    } else {
+                        showPasswordError("Password must be at least 6 characters")
+                    }
+                } else {
+                    showPhoneNumberError("Invalid Number")
+                }
+            } else {
+                showPhoneNumberError("Phone number must be 11 digits and start with '01'")")
+            }
+        }
+    }
+
+    private fun isValidPassword(password: String): Boolean {
+        return password.length >= 6
+    }
+
+    private fun isValidPhoneNumber(phoneNumber: String): Boolean {
+        return phoneNumber.length == 11 && phoneNumber.startsWith("01")
     }
 
 }

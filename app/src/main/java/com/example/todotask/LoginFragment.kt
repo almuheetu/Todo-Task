23139ef15b80c74f17c5d098012920a19a20190e
Softzino <almuheetu@gmail.com>
2024-09-13 12:03:37 +0600

    fixing togglePasswordVisible

--- app/src/main/java/com/example/todotask/LoginFragment.kt
+++ app/src/main/java/com/example/todotask/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.todotask
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -42,6 +43,9 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbarInclude.toolbarTitle.text = "Login"
         binding.toolbarInclude.backButton.visibility = View.GONE
+        binding.imageViewEye.setOnClickListener {
+            togglePasswordVisibility()
+        }
         binding.loginBtn.setOnClickListener {
             val phoneNumber = binding.phoneNumberEt.text.toString()
             val password = binding.editTextPassword.text.toString()
@@ -62,7 +66,7 @@ class LoginFragment : Fragment() {
                     showPhoneNumberError("Invalid Number")
                 }
             } else {
-                showPhoneNumberError("Phone number must be 11 digits and start with '01'")")
+                showPhoneNumberError("Phone number must be 11 digits and start with '01'")
             }
         }
     }
@@ -73,6 +77,19 @@ class LoginFragment : Fragment() {
 
     private fun isValidPhoneNumber(phoneNumber: String): Boolean {
         return phoneNumber.length == 11 && phoneNumber.startsWith("01")
+    }
+
+    private fun togglePasswordVisibility() {
+        val isPasswordVisible = binding.editTextPassword.inputType != InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+
+        if (isPasswordVisible) {
+            binding.editTextPassword.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            binding.imageViewEye.setImageResource(R.drawable.baseline_remove_red_eye_24) // Open eye icon
+        } else {
+            binding.editTextPassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            binding.imageViewEye.setImageResource(R.drawable.baseline_remove_red_eye_24) // Closed eye icon
+        }
+        binding.editTextPassword.setSelection(binding.editTextPassword.text.length) // Fix cursor position
     }
 
 }

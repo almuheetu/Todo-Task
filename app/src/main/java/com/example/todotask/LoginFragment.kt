4006fff68fee94cb70668350a9a293f07b65f8d3
Softzino <almuheetu@gmail.com>
2024-09-17 09:45:42 +0600

    project complete

--- app/src/main/java/com/example/todotask/LoginFragment.kt
+++ app/src/main/java/com/example/todotask/LoginFragment.kt
@@ -2,12 +2,14 @@ package com.example.todotask
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.todotask.databinding.FragmentLoginBinding
 
@@ -29,8 +31,19 @@ class LoginFragment : Fragment() {
         binding.toolbarInclude.toolbarTitle.text = "Login"
         binding.toolbarInclude.backButton.visibility = View.GONE
 
-        binding.imageViewEye.setOnClickListener {
-
+        binding.visibilityOn.setOnClickListener {
+            binding.visibilityOn.visibility = View.GONE
+            binding.visibilityOff.visibility = View.VISIBLE
+            binding.editTextPassword.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            binding.editTextPassword.setSelection(binding.editTextPassword.text.length)
+        }
+        binding.visibilityOff.setOnClickListener {
+            binding.visibilityOff.visibility = View.GONE
+            binding.visibilityOn.visibility = View.VISIBLE
+            binding.editTextPassword.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            binding.editTextPassword.setSelection(binding.editTextPassword.text.length)
         }
 
         binding.loginBtn.setOnClickListener {
@@ -43,10 +56,10 @@ class LoginFragment : Fragment() {
                         LoginFragmentDirections.actionLoginFragmentToTodoTaskFragmentNavId()
                     findNavController().navigate(action)
                 } else {
-                    showPasswordError("Invalid Password")
+                    Toast.makeText(requireContext(), "Incorrect Password", Toast.LENGTH_SHORT).show()
                 }
             } else {
-                showPhoneNumberError("Invalid Number")
+                Toast.makeText(requireContext(), "Username or Password is not correct...", Toast.LENGTH_SHORT).show()
             }
         }
 
